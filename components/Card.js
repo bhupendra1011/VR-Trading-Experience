6bcc564d6296bad8888376999d4f6eb217337e53
Bhupendra <bhupendra1011@gmail.com>
2021-08-29 15:16:44 +0530

    updated content & API

--- components/Card.js
+++ components/Card.js
@@ -1,144 +1,139 @@
-import React from 'react';
+import React from "react";
 import { AppRegistry, StyleSheet, Text, View, VrButton } from "react-360";
 
 export default class StockCard extends React.Component {
-    
-    symbol =  this.props.exchange ? `${this.props.exchange}:${this.props.symbol}`: this.props.symbol;
-   
-    apiKeys = [
-        'FVKV400PBKCNFBZR',
-        'YX4YAD802OGS4WIL',
-        'U4KK41787O3UIZ07',
-        'YZL3MILDPY3Y4L4M',
-        'V26027KLDLIY0BTV',
-        'O7MU5JCORTYTEBCD',
-        'W1Z2JCTMUX4KXR5J',
-        '08SPT8HBN0B66WOH',
-        'WJSZDVIH8TQYGHM8',
-        '0B0KWQXP8F94VGQK',
-        '06M5KWRGK34MXG0X',
-        '05UE8LHCXSK2IW6X',
-        'S01AOCNTFVI6GYO1',
-        '06IH36Y4BCRLEUX1',
-        'CN3P9SH8XBEQILM3',
-        '8Z9ZLOC2MA0K1NRC',
-        'QQMCUDYCRDURDV91',
-        '17XZDG71ADZJLUNM',
-        'RY9CNCIET1BVZ8T3',
-        'SEVGF67LD8DR94UM',
-        'UEM4RG2GFD5TRM6Y',
-        '9LQSP5XYP50SZR9E',
-        '38Q5V28HSSSVJTT9',
-        '0WXVR4U6PTJMOJYB',
-        'XGI2ECXS555PO44Y',
-        'VRLLQRCFPKOOK45I',
-        'B8P4EBK0YAOTMUVG'
-    ];
-    state = {
-        previousClose: 0,
-        currentPrice: 0,
-        stockData: {}
-    }
+  symbol = this.props.symbol;
 
-    currentPriceCache = [];
-    stockDataCache = [];
+  apiKeys = ["GS1YFSNPSDS729O9", "B6ZFWPJRUDS4C4QP"];
+  state = {
+    previousClose: 0,
+    currentPrice: 0,
+    stockData: {},
+  };
 
-    componentWillMount() {
-          this.fetchPreviousDayData();
-    }
+  currentPriceCache = [];
+  stockDataCache = [];
 
-    componentDidMount () {
-        this.timerId = setInterval(() => this.updateCurrentPrice(), 60000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerId);
-    }
-
-    getRandomKey() {
-         let randomInd = Number.parseInt(Math.random()*this.apiKeys.length);
-        return this.apiKeys[randomInd];
-    }
-
-    updateCurrentPrice () {
-        //  
-        let key = this.getRandomKey();
-        console.log('random key: '+key);
-        fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${this.symbol}&interval=1min&apikey=${key}`)
-        .then(results => results.json())
-            .then((data) => {
-                console.log(`Updating ${this.symbol} price`);
-                let series = data["Time Series (1min)"];
-                const keys = Object.keys(series);
-                let todayPrice;
-                if(series) {
-                    todayPrice = Number.parseFloat(series[keys[0]]["1. open"], 10);
-                    this.currentPriceCache.push(todayPrice);
-                    this.stockDataCache.push(data); 
-        
-                } else {
-                    console.log(data);
-                    //no data from API, use cached data
-                    
-                    //console.log(this.currentPriceCache);
-                    let length = this.currentPriceCache.length;
-                    if (length) {
-                        let randomInd = Number.parseInt(Math.random()*length);
-                        todayPrice = this.currentPriceCache[randomInd];
-                        data = this.stockDataCache[randomInd];
-                    }
-                }
-                if(todayPrice) {
-                    this.setState({ currentPrice: todayPrice, stockData: data });
-                }
-            });
-    }
-
-    fetchPreviousDayData () {
-        let key = 'MNEDEDZUGFB50C92';
-        fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.symbol}&interval=1min&apikey=${key}`)
-        .then(results => results.json())
-            .then((data) => {
-                let series = data["Time Series (Daily)"];
-                const keys = Object.keys(series);
-                let previousClose = Number.parseFloat(series[keys[1]]["4. close"], 10);
-                this.setState({ previousClose: previousClose });
-                this.updateCurrentPrice ();           
-            });
-    }
-
-    render () {
-      return (
-
-         <VrButton onClick={() => this.props.handleStockSelection({activeStock: this.props.symbol, activeStockData: this.state.stockData })}>
-          <View style={[styles.stockBox, styles.alignCenter, this.state.currentPrice > this.state.previousClose ? styles.gainers : styles.losers]}>
-            <Text > {this.props.symbol} </Text>
-            <Text>{this.state.currentPrice.toFixed(2)}</Text>
-            <Text> { ((this.state.currentPrice - this.state.previousClose)*100/this.state.previousClose).toFixed(2)}% </Text>
-          </View>
-         </VrButton>
-      )
+  componentWillMount() {
+    this.fetchPreviousDayData();
+    this.updateCurrentPrice();
   }
-  } 
-  const styles = StyleSheet.create({
-    alignCenter: {
-      display: "flex",
-      justifyContent: 'center',
-      alignItems: 'center'
-  
-    },
-    stockBox: {
-      width: 150,
-      height: 150,
-      margin: 10
-    },
 
-    gainers: {
-      backgroundColor: "#509d0a",
-    },
+  componentDidMount() {
+    // this.timerId = setInterval(() => this.updateCurrentPrice(), 60000);
+  }
 
-    losers: {
-        backgroundColor: "#d22222f7"
-      }
-  
-  });
+  componentWillUnmount() {
+    // clearInterval(this.timerId);
+  }
+
+  getRandomKey() {
+    let randomInd = Number.parseInt(Math.random() * this.apiKeys.length);
+    return this.apiKeys[randomInd];
+  }
+
+  updateCurrentPrice() {
+    //
+    // let key = this.getRandomKey();
+    // console.log("random key: " + key);
+    let key = "GS1YFSNPSDS729O9";
+
+    fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${this.symbol}&interval=1min&apikey=${key}`
+    )
+      .then((results) => results.json())
+      .then((data) => {
+        console.log(`Updating ${this.symbol} price`);
+        let series = data["Time Series (1min)"];
+        const keys = Object.keys(series);
+        let todayPrice;
+        if (series) {
+          todayPrice = Number.parseFloat(series[keys[0]]["1. open"], 10);
+          this.currentPriceCache.push(todayPrice);
+          this.stockDataCache.push(data);
+        } else {
+          console.log(data);
+          //no data from API, use cached data
+
+          //console.log(this.currentPriceCache);
+          let length = this.currentPriceCache.length;
+          if (length) {
+            let randomInd = Number.parseInt(Math.random() * length);
+            todayPrice = this.currentPriceCache[randomInd];
+            data = this.stockDataCache[randomInd];
+          }
+        }
+        if (todayPrice) {
+          this.setState({ currentPrice: todayPrice, stockData: data });
+        }
+      });
+  }
+
+  fetchPreviousDayData() {
+    // let key = this.getRandomKey();
+    let key = "B6ZFWPJRUDS4C4QP";
+    fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.symbol}&interval=5min&apikey=${key}`
+    )
+      .then((results) => results.json())
+      .then((data) => {
+        let series = data["Time Series (Daily)"];
+        const keys = Object.keys(series);
+        let previousClose = Number.parseFloat(series[keys[1]]["4. close"], 10);
+        this.setState({ previousClose: previousClose });
+        // this.updateCurrentPrice();
+      });
+  }
+
+  render() {
+    const { previousClose = 0, currentPrice = 0 } = this.state;
+    if (!previousClose || !currentPrice) return null;
+    const diff = currentPrice - previousClose;
+    const change = diff ? (diff / previousClose).toFixed(2) * 100 : 0;
+
+    return (
+      <VrButton
+        onClick={() =>
+          this.props.handleStockSelection({
+            activeStock: this.props.symbol,
+            activeStockData: this.state.stockData,
+          })
+        }
+      >
+        <View
+          style={[
+            styles.stockBox,
+            styles.alignCenter,
+            this.state.currentPrice > this.state.previousClose
+              ? styles.gainers
+              : styles.losers,
+          ]}
+        >
+          <Text> {this.props.symbol} </Text>
+          <Text>{this.state.currentPrice.toFixed(2)}</Text>
+          <Text>{change}% </Text>
+        </View>
+      </VrButton>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  alignCenter: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  stockBox: {
+    width: 150,
+    height: 150,
+    margin: 10,
+  },
+
+  gainers: {
+    backgroundColor: "#509d0a",
+  },
+
+  losers: {
+    backgroundColor: "#d22222f7",
+  },
+});
--- index.js
+++ index.js
@@ -9,193 +9,213 @@ import StocksChart from "./components/graph";
 import NewsPanel from "./components/NewsPanel";
 
 export default class vr_trading extends React.Component {
-	state = {
-		activeStock: "",
-		accountBalance: 10000,
-    	activeStockData: null,
-		activeStockInfo: {
-			symbol: "",
-			open: "",
-			close: "",
-			high: "",
-			low: "",
-			volume: ""
-		}
-	};
-	defaultQuantity = 10;
+  state = {
+    activeStock: "",
+    accountBalance: 10000,
+    activeStockData: null,
+    activeStockInfo: {
+      symbol: "",
+      open: "",
+      close: "",
+      high: "",
+      low: "",
+      volume: "",
+    },
+  };
+  defaultQuantity = 10;
 
-	handleStockSelection(nextState) {
-		this.setState({ 
-			activeStock: nextState.activeStock,
-			activeStockData: nextState.activeStockData, 
-			activeStockInfo: this.getActiveStockInfo(nextState)
-		});
-	}
+  handleStockSelection(nextState) {
+    this.setState({
+      activeStock: nextState.activeStock,
+      activeStockData: nextState.activeStockData,
+      activeStockInfo: this.getActiveStockInfo(nextState),
+    });
+  }
 
-	getActiveStockInfo(nextState) {
-		let data = nextState.activeStockData;
-		// if (!data) return;
-		const timeSeries = data["Time Series (1min)"];
-		const metaData = data["Meta Data"];
-		const keys = Object.keys(timeSeries);
-		const latestData = timeSeries[keys[1]];
-		return {
-			symbol: metaData["2. Symbol"],
-			open: Number(latestData["1. open"]).toFixed(2),
-			close: Number(latestData["4. close"]).toFixed(2),
-			high: Number(latestData["2. high"]).toFixed(2),
-			low: Number(latestData["3. low"]).toFixed(2),
-			volume: Number(latestData["5. volume"]).toFixed(2)
-		};
-	} 
+  getActiveStockInfo(nextState) {
+    let data = nextState.activeStockData;
+    // if (!data) return;
+    const timeSeries = data["Time Series (1min)"];
+    const metaData = data["Meta Data"];
+    const keys = Object.keys(timeSeries);
+    const latestData = timeSeries[keys[1]];
+    return {
+      symbol: metaData["2. Symbol"],
+      open: Number(latestData["1. open"]).toFixed(2),
+      close: Number(latestData["4. close"]).toFixed(2),
+      high: Number(latestData["2. high"]).toFixed(2),
+      low: Number(latestData["3. low"]).toFixed(2),
+      volume: Number(latestData["5. volume"]).toFixed(2),
+    };
+  }
 
-	getStockPrice () {
-		const series = this.state.activeStockData["Time Series (1min)"];
-		const keys = Object.keys(series);
-		return Number.parseFloat(series[keys[0]]["1. open"], 10);
-	}
+  getStockPrice() {
+    const series = this.state.activeStockData["Time Series (1min)"];
+    const keys = Object.keys(series);
+    return Number.parseFloat(series[keys[0]]["1. open"], 10);
+  }
 
-	buyStock () {
-		const price = this.getStockPrice();
-		this.setState({accountBalance: this.state.accountBalance - (price * this.defaultQuantity)});
-	}
+  buyStock() {
+    const price = this.getStockPrice();
+    this.setState({
+      accountBalance: this.state.accountBalance - price * this.defaultQuantity,
+    });
+  }
 
-	sellStock() {
-		const price = this.getStockPrice();
-		this.setState({accountBalance: this.state.accountBalance + (price * this.defaultQuantity)});
-	}
+  sellStock() {
+    const price = this.getStockPrice();
+    this.setState({
+      accountBalance: this.state.accountBalance + price * this.defaultQuantity,
+    });
+  }
 
-	render() {
-		console.log('Render called');
-		console.log(this.state.activeStock);
-		let stockInfo;
-		if(this.state.activeStock) {
-			stockInfo = <View>
-							<StocksChart data={this.state.activeStockInfo} />
-							<View style={styles.buttonBox}>
-								<Text style={styles.bigFont}>Quantity: {this.defaultQuantity}</Text>
-								<VrButton onClick={this.buyStock.bind(this)}>
-									<View style={[styles.button, styles.buttonBuy, styles.alignCenter]}>
-										<Text style={styles.bigFont}>BUY</Text>
-									</View>
-								</VrButton>
-								<VrButton onClick={this.sellStock.bind(this)}>
-									<View style={[styles.button, styles.buttonSell, styles.alignCenter]}>
-										<Text style={styles.bigFont}>SELL</Text>
-									</View>
-								</VrButton>
-							</View>
-						</View>;
-		} else {
-			stockInfo = <View style={[styles.graphContainer, styles.alignCenter]}>
-				<Text style={styles.bigFont}>No stock selected! </Text>
-			</View>
-		}
-		return (
-			<View style={styles.panel}>
-				<View style={styles.panelBox}>
-					<Text style={styles.bigFont}> IT Stocks </Text>
-					<StockCard symbol='INFY' exchange='NSE' handleStockSelection={this.handleStockSelection.bind(this)} />
-					<StockCard symbol='TCS' exchange='NSE' handleStockSelection={this.handleStockSelection.bind(this)} />
-				</View>
+  render() {
+    console.log("Render called");
+    console.log(this.state.activeStock);
+    let stockInfo;
+    if (this.state.activeStock) {
+      stockInfo = (
+        <View>
+          <StocksChart data={this.state.activeStockInfo} />
+          <View style={styles.buttonBox}>
+            <Text style={styles.bigFont}>Quantity: {this.defaultQuantity}</Text>
+            <VrButton onClick={this.buyStock.bind(this)}>
+              <View
+                style={[styles.button, styles.buttonBuy, styles.alignCenter]}
+              >
+                <Text style={styles.bigFont}>BUY</Text>
+              </View>
+            </VrButton>
+            <VrButton onClick={this.sellStock.bind(this)}>
+              <View
+                style={[styles.button, styles.buttonSell, styles.alignCenter]}
+              >
+                <Text style={styles.bigFont}>SELL</Text>
+              </View>
+            </VrButton>
+          </View>
+        </View>
+      );
+    } else {
+      stockInfo = (
+        <View style={[styles.graphContainer, styles.alignCenter]}>
+          <Text style={styles.bigFont}>No stock selected! </Text>
+        </View>
+      );
+    }
+    return (
+      <View style={styles.panel}>
+        <View style={styles.panelBox}>
+          <Text style={styles.bigFont}> IT Stocks </Text>
+          <StockCard
+            symbol="INFY"
+            exchange="NSE"
+            handleStockSelection={this.handleStockSelection.bind(this)}
+          />
+        </View>
 
-				<View style={styles.centerPanel}>
-					<View style={[styles.funds, styles.alignCenter]}>
-						<Text style={styles.bigFont}>Funds: {this.state.accountBalance.toFixed(2)}</Text>
-					</View>
-					{stockInfo}
-				</View>
+        <View style={styles.centerPanel}>
+          <View style={[styles.funds, styles.alignCenter]}>
+            <Text style={styles.bigFont}>
+              Funds: {this.state.accountBalance.toFixed(2)}
+            </Text>
+          </View>
+          {stockInfo}
+        </View>
 
-				<View style={styles.panelBox}>
-					<Text> BANK Stocks </Text>
-					<StockCard symbol="SBIN" exchange="NSE" handleStockSelection={this.handleStockSelection.bind(this)} />
-					<StockCard symbol="YESBANK" exchange="NSE" handleStockSelection={this.handleStockSelection.bind(this)} />
-				</View>
-			</View>
-		);
-	}
+        <View style={styles.panelBox}>
+          <Text> BANK Stocks </Text>
+          <StockCard
+            symbol="SBI"
+            exchange="NSE"
+            handleStockSelection={this.handleStockSelection.bind(this)}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 // Stocks panel , need to be moved
 
 const styles = StyleSheet.create({
-	panel: {
-		// Fill the entire surface
-		width: 1000,
-		height: 600,
-		backgroundColor: "rgba(255, 255, 255, 0.4)",
-		flexDirection: "row",
-		justifyContent: "space-between"
-	},
-	centerPanel: {
-		flexDirection: "column",
-		alignItems: 'center'
-	},
-	funds: {
-		width: 300,
-		height: 100,
-		backgroundColor: '#545454',
-		marginBottom: 10
-	},
-	graphContainer: {
-		width: 400,
-		height: 200,
-		borderWidth: 2,
-		borderColor: "black",
-		backgroundColor: "#24292e",
-		justifyContent: 'space-around'
-	},
-	panelBox: {
-		padding: 20,
-		backgroundColor: "#5373a3",
-		borderColor: "#639dda",
-		borderWidth: 2
-	},
-	buttonBox: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-around',
-		marginTop: 10,
-		backgroundColor: "#24292e",
-		width: 400,
-		height: 100 
-	},
-	button: {
-		width: 80,
-		height: 80
-	},
-	buttonBuy: {
-		backgroundColor: 'blue'
-	},
-	buttonSell: {
-		backgroundColor: 'red'
-	},
-	alignCenter: {
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center"
-	},
-	stockBox: {
-		width: 80,
-		height: 80
-	},
-	bigFont: {
-		fontSize: 20,
-		fontWeight: 'bold'
-	},
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: "rgba(255, 255, 255, 0.4)",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  centerPanel: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  funds: {
+    width: 300,
+    height: 100,
+    backgroundColor: "#545454",
+    marginBottom: 10,
+  },
+  graphContainer: {
+    width: 400,
+    height: 200,
+    borderWidth: 2,
+    borderColor: "black",
+    backgroundColor: "#24292e",
+    justifyContent: "space-around",
+  },
+  panelBox: {
+    padding: 20,
+    backgroundColor: "#5373a3",
+    borderColor: "#639dda",
+    borderWidth: 2,
+  },
+  buttonBox: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    marginTop: 10,
+    backgroundColor: "#24292e",
+    width: 400,
+    height: 100,
+  },
+  button: {
+    width: 80,
+    height: 80,
+  },
+  buttonBuy: {
+    backgroundColor: "blue",
+  },
+  buttonSell: {
+    backgroundColor: "red",
+  },
+  alignCenter: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  stockBox: {
+    width: 80,
+    height: 80,
+  },
+  bigFont: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
 
-	gainers: {
-		backgroundColor: "#509d0a"
-	},
-	losers: {
-		backgroundColor: "#d22222f7"
-	},
-	hide: {
-		opacity: 0
-	},
-	show: {
-		opacity: 1
-	}
+  gainers: {
+    backgroundColor: "#509d0a",
+  },
+  losers: {
+    backgroundColor: "#d22222f7",
+  },
+  hide: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+  },
 });
 
 AppRegistry.registerComponent("vr_trading", () => vr_trading);
